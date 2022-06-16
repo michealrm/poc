a6695f23471a23643fc8e7f23b92f2e9ed12fcc1
Mike M <michealrmyers@gmail.com>
2022-06-16 09:50:15 -0500

    Initial Commit

---
+++ poc/pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocApp.swift
+//  poc
+//
+//  Created by Micheal Myers on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
